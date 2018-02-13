41fe24ef185dbc1b6f728740f4bd939dd41e2815
schomy <sebastien.chomy@gmail.com>
2018-02-13 14:35:39 +0100

    #1, Add dependency Webpack Encore and init parameters

---
+++ assets/js/global.js
@@ -0,0 +1,11 @@
+/**
+ * This file is part of oc_snowtricks project
+ *
+ * @author: Sébastien CHOMY <sebastien.chomy@gmail.com>
+ * @since 2018/02
+ */
+require('../css/global.scss');
+
+// loads the jquery package from node_modules
+var $ = require('jquery');
+
