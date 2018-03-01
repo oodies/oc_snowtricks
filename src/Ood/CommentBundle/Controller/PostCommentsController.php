d855314725fab95344a2e97a16cd42d19eb82c18
schomy <sebastien.chomy@gmail.com>
2018-03-01 22:55:29 +0100

    #20 Update implementation

--- src/Ood/CommentBundle/Controller/PostCommentsController.php
+++ src/Ood/CommentBundle/Controller/PostCommentsController.php
@@ -8,12 +8,10 @@
 
 namespace Ood\CommentBundle\Controller;
 
-use Ood\BlogpostBundle\Entity\Post;
 use Ood\CommentBundle\Entity\Comment;
 use Ood\CommentBundle\Entity\Thread;
 use Ood\CommentBundle\Form\CommentType;
 use Ood\UserBundle\Entity\User;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,23 +26,37 @@ class PostCommentsController extends Controller
 {
     /**
      * @param Request $request
-     * @param Post    $post
-     *
-     * @ParamConverter("post", options={"id"="postId"})
      *
      * @return \Symfony\Component\HttpFoundation\Response | \Symfony\Component\HttpFoundation\JsonResponse;
      * @throws \LogicException
      */
-    public function postCommentsAction(Request $request, Post $post)
+    public function postCommentsAction(Request $request)
     {
+        $threadId = $request->get('threadId');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $repositoryThread = $em->getRepository('OodCommentBundle:Thread');
+        /** @var Thread $thread */
+        $thread = $repositoryThread->find($threadId);
+
+        if (is_null($thread)) {
+            $thread = new Thread();
+            $thread->setIdThread($threadId);
+            $em->persist($thread);
+            $em->flush();
+        }
+
         $comment = new Comment();
+        $comment->setThread($thread);
+
         $form = $this->createForm(
             CommentType::class,
             $comment,
             [
                 'action' => $this->generateUrl(
                     'ood_comment_postComments_postComments',
-                    ['postId' => $post->getIdPost()]
+                    ['threadId' => $thread->getIdThread()]
                 ),
                 'method' => 'POST'
             ]
@@ -52,22 +64,9 @@ class PostCommentsController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-
             /** @var User $user */
             $user = $this->getUser();
             $comment->setAuthor($user);
-
-            $repositoryThread = $em->getRepository('OodCommentBundle:Thread');
-            /** @var Thread $thread */
-            $thread = $repositoryThread->findOneBy(['post' => $post]);
-
-            if (is_null($thread)) {
-                $thread = new Thread();
-                $thread->setPost($post);
-                $em->persist($thread);
-            }
-            $comment->setThread($thread);
 
             $em->persist($comment);
             $em->flush();
