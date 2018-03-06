6dd4fedebef5988b56252bdfe17e3bfaf3e0a9fc
schomy <sebastien.chomy@gmail.com>
2018-03-06 11:52:30 +0100

    #31, Upgrade Image Entity + Add ImageType (form)

--- src/Ood/PictureBundle/Entity/Image.php
+++ src/Ood/PictureBundle/Entity/Image.php
@@ -9,7 +9,7 @@
 namespace Ood\PictureBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * Class Image
@@ -18,14 +18,13 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @ORM\Table(name="picture_image")
  * @ORM\Entity(repositoryClass="Ood\PictureBundle\Repository\ImageRepository")
+ * @ORM\HasLifecycleCallbacks()
  */
 class Image
 {
-    /*
-     * *******************************
+    /* *******************************
      * PROPERTIES ORM
-     * ******************************
-     */
+     **/
 
     /**
      * @var int
@@ -40,7 +39,7 @@ class Image
      *
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $idImage;
+    protected $idImage;
 
     /**
      * The image file name extension
@@ -53,17 +52,8 @@ class Image
      *     length=255,
      *     options={"comment"="The image file name extension"}
      * )
-     *
-     * @Assert\NotBlank(
-     *     message="image.extension.not_blank"
-     *     )
-     *
-     * @Assert\Length(
-     *     max="255",
-     *     maxMessage="image.extension.max_length"
-     * )
      */
-    private $extension = "";
+    protected $extension;
 
     /**
      * The alternative text describing the image to attribute alt of the HTML <img> element.
@@ -76,22 +66,25 @@ class Image
      *     length=255,
      *     options={"comment"="The alternative text describing the image to attribute alt of the HTML <img> element."}
      * )
-     *
-     * @Assert\NotBlank(
-     *     message="image.alt.not_blank"
-     *     )
-     *
-     * @Assert\Length(
-     *     max="255",
-     *     maxMessage="image.alt.max_length"
-     * )
      */
-    private $alt = "";
+    protected $alt;
 
-    /*
-     * *******************************
+    /* *******************************
+     * OTHERS PROPERTIES
+     */
+
+    /**
+     * @var UploadedFile
+     */
+    protected $file;
+
+    /**
+     * @var string
+     */
+    protected $tempFilename;
+
+    /* *******************************
      * GETTER / SETTER
-     * ******************************
      */
 
     /**
@@ -150,5 +143,113 @@ class Image
         $this->alt = $alt;
 
         return $this;
+    }
+
+    /* *******************************
+     * METHODS
+     */
+
+    /**
+     * get file
+     *
+     * @return null|UploadedFile
+     */
+    public function getFile(): ?UploadedFile
+    {
+        return $this->file;
+    }
+
+    /**
+     * set file
+     *
+     * @param null|UploadedFile $file
+     */
+    public function setFile(?UploadedFile $file = null)
+    {
+        $this->file = $file;
+        // Reset attributes
+        $this->extension = null;
+        $this->alt = null;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        if (null != $this->file) {
+            $this->extension = $this->file->guessExtension();
+            $this->alt = $this->file->getClientOriginalName();
+        }
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     *
+     * @throws \Symfony\Component\HttpFoundation\File\Exception\FileException
+     */
+    public function upload()
+    {
+        // No file to process
+        if (null == $this->file) {
+            return;
+        }
+
+        $this->file->move(
+            $this->getUploadRootDir(),
+            $this->idImage . '.' . $this->extension
+        );
+    }
+
+    /**
+     * @ORM\PreRemove()
+     */
+    public function preRemoveFile()
+    {
+        // I save the filename that is built from the database information
+        $this->tempFilename = $this->getUploadRootDir() . '/' . $this->idImage . '.' . $this->extension;
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeFile()
+    {
+        if (file_exists($this->tempFilename)) {
+            unlink($this->tempFilename);
+        }
+    }
+
+    /**
+     * Image url for a browser (relative to the /web folder)
+     *
+     * @return string
+     */
+    public function getWebPath()
+    {
+        return $this->getWebBase() . '/' . $this->getIdImage() . '.' . $this->getExtension();
+    }
+
+    /**
+     * Images folder for a browser (relative to the /web folder)
+     *
+     * @return string
+     */
+    protected function getWebBase()
+    {
+        return 'img/picture';
+    }
+
+    /**
+     * Images folder for backend code
+     *
+     * @return string
+     */
+    protected function getUploadRootDir()
+    {
+        // TODO I Should use Kernel.project_dir
+        return dirname(dirname(dirname(dirname(__DIR__)))) . '/web/' . $this->getWebBase();
     }
 }
---
+++ src/Ood/PictureBundle/Form/ImageType.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This file is part of oc_snowtricks project
+ *
+ * @author: Sébastien CHOMY <sebastien.chomy@gmail.com>
+ * @since 2018/02
+ */
+
+namespace Ood\PictureBundle\Form;
+
+use Ood\PictureBundle\Entity\Image;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image as ValidatorImage;
+
+/**
+ * Class ImageType
+ *
+ * @package
+ */
+class ImageType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('file', FileType::class,
+                [
+                    'required'    => false,
+                    'attr'        => [
+                        'accept' => 'image/*;capture=camcorder',
+                        'class'  => 'input_file',
+                    ],
+                    'label'       => 'Documents...',
+                    'label_attr'  => [
+                        'class'    => 'input_file-trigger',
+                        'tabindex' => 0
+                    ],
+                    'constraints' => [
+                        new ValidatorImage([
+                            'maxSize'          => '1M',
+                        ])
+                    ]
+                ]);
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     *
+     * @throws \Symfony\Component\OptionsResolver\Exception\AccessException
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class'         => Image::class,
+                'translation_domain' => 'application',
+            ]
+        );
+    }
+}
